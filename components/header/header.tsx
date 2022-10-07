4b67e09bf619c6bd84d3ec95b1a76947039753b8
sabesh <sabesh5601@gmail.com>
2022-10-07 14:27:30 +0530

    header menu bar

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import Button from "../button";
@@ -7,57 +7,66 @@ const Header = () => {
   const nav = useRouter();
 
   const [showingMenu, setShowingMenu] = useState(false);
+
+  useEffect(() => {
+    if (showingMenu) {
+      document.body.classList.add("stop-scrolling");
+    } else {
+      document.body.classList.remove("stop-scrolling");
+    }
+  }, [showingMenu]);
+
   return (
-    <header className="flex fixed top-0 bg-black/30 backdrop-blur-lg z-10 text-white w-full justify-between py-4 px-4 xl:px-10 lg:py-6 items-center">
+    <header className="flex fixed top-0 bg-black/30 backdrop-blur-lg z-10 menu-nav text-white w-full justify-between py-4 px-4 xl:px-10 lg:py-6 items-center">
       <div className="w-full lg:w-1/4 flex justify-between lg:justify-start items-center">
         {showingMenu && (
-          <div className="absolute duration-300 transition-all flex flex-col z-30 w-full left-0 top-20 items-center justify-center shadow-xl border-[0.02rem] border-gray-400 border-opacity-20 bg-black/20 rounded-md lg:rounded-xl backdrop-blur-lg">
+          <div className="absolute duration-300 transition-all flex flex-col z-30 w-full left-0 top-0 h-screen items-center justify-center shadow-xl border-[0.02rem] border-gray-400 border-opacity-20 bg-gray-900 rounded-md lg:rounded-xl">
             <div
               onClick={() => setShowingMenu(false)}
-              className="font-ubuntu text-center my-4 text-xl"
+              className="font-ubuntu text-center my-6 text-xl"
             >
               <Link href="/">Home</Link>
             </div>
             <div
               onClick={() => setShowingMenu(false)}
-              className="font-ubuntu text-center my-4 text-xl"
+              className="font-ubuntu text-center my-6 text-xl"
             >
               <Link href="/coming-soon">Events</Link>
             </div>
             <div
               onClick={() => setShowingMenu(false)}
-              className="font-ubuntu text-center my-4 text-xl"
+              className="font-ubuntu text-center my-6 text-xl"
             >
               <Link href="/coming-soon">Workshops</Link>
             </div>
             <div
               onClick={() => setShowingMenu(false)}
-              className="font-ubuntu text-center my-4 text-xl"
+              className="font-ubuntu text-center my-6 text-xl"
             >
               <Link href="/coming-soon">Hackathon</Link>
             </div>
             <div
               onClick={() => setShowingMenu(false)}
-              className="font-ubuntu text-center my-4 text-xl"
+              className="font-ubuntu text-center my-6 text-xl"
             >
               <Link href="/coming-soon">Schedule</Link>
             </div>
             <div
               onClick={() => setShowingMenu(false)}
-              className="font-ubuntu text-center my-4 text-xl"
+              className="font-ubuntu text-center my-6 text-xl"
             >
               <Link href="/coming-soon">Sponsors</Link>
             </div>
             <div
               onClick={() => setShowingMenu(false)}
-              className="font-ubuntu text-center my-4 text-xl"
+              className="font-ubuntu text-center my-6 text-xl"
             >
               <Link href="/coming-soon">Passes</Link>
             </div>
           </div>
         )}
         <i
-          className="w-1/6 lg:hidden cursor-pointer"
+          className="w-1/6 z-50 lg:hidden cursor-pointer"
           onClick={() => setShowingMenu(!showingMenu)}
           dangerouslySetInnerHTML={{
             __html: `<ion-icon

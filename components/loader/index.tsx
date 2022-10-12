0476cc8d9ad7044f55c4a482fa7cd58b63ce55c9
Sabesh Bharathi <sabesh@rocketlane.com>
2022-10-12 22:19:14 +0530

    Loaders!!! Yo!!!

---
+++ components/loader/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Loader = () => {
+  return (
+    <span className="h-8 w-8 block rounded-full border-4 border-t-sky-600 animate-spin opacity-70"></span>
+  );
+};
+export default Loader;
--- pages/events/[name].tsx
+++ pages/events/[name].tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import BlurryCircle from "../../components/blurry-circle";
 import Footer from "../../components/footer";
 import Header from "../../components/header/header";
+import Loader from "../../components/loader";
 import VectorsBG from "../../components/vectors-bg";
 import { Department, Event, EventType } from "../../types";
 
@@ -48,6 +49,7 @@ const EventPage = () => {
   });
   const router = useRouter();
   const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const [department, setDepartment] = useState("");
 
@@ -57,18 +59,14 @@ const EventPage = () => {
   }, [router]);
 
   useEffect(() => {
-    console.log(selectedEvent);
-
     if (selectedEvent != null) {
       setSelectedEventDetails(events[selectedEvent]);
-      console.log("yooo");
     }
   }, [selectedEvent, events, department]);
 
   useEffect(() => {
     if (department != "") {
       const res = axios.get(`/api/events/${department}`).then((res) => {
-        console.log(res.data.events);
         const validEvents = [];
         for (const event of res.data.events) {
           if (event.description) {
@@ -79,6 +77,7 @@ const EventPage = () => {
         }
         setEvents(validEvents);
         setSelectedEvent(0);
+        setLoading(false);
       });
     }
   }, [department]);
@@ -89,128 +88,140 @@ const EventPage = () => {
       <VectorsBG />
       <BlurryCircle />
       <h1 className="text-3xl text-white font-azonix text-center mt-40">
-        Department of {department.split("-").join(" ")}{" "}
+        Department of {department?.split("-").join(" ")}{" "}
         {department != "information-technology" &&
         department != "snu-commerce" &&
         department.split("-")[department.split("-").length - 1] != "engineering"
           ? "Engineering"
           : ""}
       </h1>
-      <div className="flex flex-col lg:flex-row w-11/12 lg:w-5/6 mx-auto gap-10 my-10">
-        <div className="text-white lg:overflow-auto overflow-scroll lg:p-10 rounded-lg lg:w-1/4  backdrop-blur-md bg-black/30 border-[0.02rem] border-gray-400 border-opacity-20">
-          <ul className="flex lg:flex-col px-2 gap-10 lg:gap-0 flex-row w-full justify-between">
-            {events.map((event, i) => (
-              <li
-                className="font-ubuntu min-w-fit lg:w-auto font-thin flex items-center cursor-pointer my-10 text-xl transition-all duration-300 hover:font-bold"
-                key={i}
-                onClick={() => setSelectedEvent(i)}
-              >
-                <i
-                  className="text-2xl mt-1"
-                  dangerouslySetInnerHTML={{
-                    __html: `<ion-icon
+      {loading && (
+        <div className="flex items-center justify-center my-40">
+          <Loader />
+        </div>
+      )}
+      {!loading && (
+        <div className="flex flex-col lg:flex-row w-11/12 lg:w-5/6 mx-auto gap-10 my-10">
+          <div className="text-white lg:overflow-auto overflow-scroll lg:p-10 rounded-lg lg:w-1/4  backdrop-blur-md bg-black/30 border-[0.02rem] border-gray-400 border-opacity-20">
+            <ul className="flex lg:flex-col px-2 gap-10 lg:gap-0 flex-row w-full justify-between">
+              {events.map((event, i) => (
+                <li
+                  className="font-ubuntu min-w-fit lg:w-auto font-thin flex items-center cursor-pointer my-10 text-xl transition-all duration-300 hover:font-bold"
+                  key={i}
+                  onClick={() => setSelectedEvent(i)}
+                >
+                  <i
+                    className="text-2xl mt-1"
+                    dangerouslySetInnerHTML={{
+                      __html: `<ion-icon
                   name="caret-forward-outline"
                 ></ion-icon>`,
-                  }}
-                ></i>
-                {event.event_name}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="text-white p-10 rounded-lg lg:w-3/4  backdrop-blur-md bg-black/30 border-[0.02rem] border-gray-400 border-opacity-20">
-          <h3 className="font-ubuntu font-bold text-2xl">
-            {selectedEventDetails?.event_name}
-          </h3>
-          <p className="font-ubuntu mt-8">
-            {selectedEventDetails?.description}
-          </p>
-          <div className="flex gap-10 justify-between items-center my-10">
-            <div className="text-center w-1/4">
-              <p className="font-ubuntu mb-2">Team Type</p>
-              <i
-                dangerouslySetInnerHTML={{
-                  __html: `<ion-icon
+                    }}
+                  ></i>
+                  {event.event_name}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="text-white p-10 rounded-lg lg:w-3/4  backdrop-blur-md bg-black/30 border-[0.02rem] border-gray-400 border-opacity-20">
+            <h3 className="font-ubuntu font-bold text-2xl">
+              {selectedEventDetails?.event_name}
+            </h3>
+            <p className="font-ubuntu mt-8">
+              {selectedEventDetails?.description}
+            </p>
+            <div className="grid grid-cols-2 gap-y-10 lg:gap-y-0 place-items-center lg:flex lg:gap-10 justify-between items-center my-10">
+              <div className="text-center lg:w-1/4">
+                <p className="font-ubuntu mb-2">Team Type</p>
+                <i
+                  dangerouslySetInnerHTML={{
+                    __html: `<ion-icon
                   name="person-circle-outline"
                   size="large"
                 ></ion-icon>`,
-                }}
-              ></i>
-              <p className="font-ubuntu">
-                {selectedEventDetails?.type ?? "TBD"}
-              </p>
-            </div>
-            <div className="text-center w-1/4">
-              <p className="font-ubuntu mb-2">Team Size</p>
-              <i
-                dangerouslySetInnerHTML={{
-                  __html: `<ion-icon
+                  }}
+                ></i>
+                <p className="font-ubuntu">
+                  {selectedEventDetails?.type ?? "TBD"}
+                </p>
+              </div>
+              <div className="text-center lg:w-1/4">
+                <p className="font-ubuntu mb-2">Team Size</p>
+                <i
+                  dangerouslySetInnerHTML={{
+                    __html: `<ion-icon
                   name="people-circle-outline"
                   size="large"
                 ></ion-icon>`,
-                }}
-              ></i>
-              <p className="font-ubuntu">
-                {selectedEventDetails?.size ?? "TBD"}
-              </p>
-            </div>
-            <div className="text-center w-1/4">
-              <p className="font-ubuntu mb-2">Venue</p>
-              <i
-                dangerouslySetInnerHTML={{
-                  __html: `<ion-icon
+                  }}
+                ></i>
+                <p className="font-ubuntu">
+                  {selectedEventDetails?.size ?? "TBD"}
+                </p>
+              </div>
+              <div className="text-center lg:w-1/4">
+                <p className="font-ubuntu mb-2">Venue</p>
+                <i
+                  dangerouslySetInnerHTML={{
+                    __html: `<ion-icon
                   name="navigate-circle-outline"
                   size="large"
                 ></ion-icon>`,
-                }}
-              ></i>
-              <p className="font-ubuntu">{selectedEventDetails?.venue}</p>
-            </div>
-            <div className="text-center w-1/4">
-              <p className="font-ubuntu mb-2">Time</p>
-              <i
-                dangerouslySetInnerHTML={{
-                  __html: `<ion-icon
+                  }}
+                ></i>
+                <p className="font-ubuntu">{selectedEventDetails?.venue}</p>
+              </div>
+              <div className="text-center lg:w-1/4">
+                <p className="font-ubuntu mb-2">Time</p>
+                <i
+                  dangerouslySetInnerHTML={{
+                    __html: `<ion-icon
                   name="alarm-outline"
                   size="large"
                 ></ion-icon>`,
-                }}
-              ></i>
+                  }}
+                ></i>
 
-              <p className="font-ubuntu">{selectedEventDetails?.time}</p>
+                <p className="font-ubuntu">{selectedEventDetails?.time}</p>
+              </div>
             </div>
+            {selectedEventDetails?.sections &&
+              selectedEventDetails?.sections?.values?.map(
+                (
+                  section: { sectionData: string; sectionName: string },
+                  i: any
+                ) => {
+                  const dataString = section.sectionData;
+                  const dataPieces = dataString.split("#");
+
+                  return (
+                    <>
+                      <p className="font-ubuntu text-xl font-semibold mt-4 mb-2">
+                        {section?.sectionName}
+                      </p>
+                      {dataPieces.map((piece, i) => {
+                        if (dataPieces.length == 1) {
+                          return (
+                            <p className="font-ubuntu text-lg" key={i}>
+                              {piece}
+                            </p>
+                          );
+                        }
+
+                        if (piece != "")
+                          return (
+                            <li className="font-ubuntu font-thin my-2 bulls">
+                              {piece}
+                            </li>
+                          );
+                      })}
+                    </>
+                  );
+                }
+              )}
           </div>
-          {selectedEventDetails?.sections &&
-            selectedEventDetails?.sections?.values?.map((section, i) => {
-              const dataString = section.sectionData;
-              const dataPieces = dataString.split("#");
-
-              return (
-                <>
-                  <p className="font-ubuntu text-xl font-semibold mt-4 mb-2">
-                    {section?.sectionName}
-                  </p>
-                  {dataPieces.map((piece, i) => {
-                    if (dataPieces.length == 1) {
-                      return (
-                        <p className="font-ubuntu text-lg" key={i}>
-                          {piece}
-                        </p>
-                      );
-                    }
-
-                    if (piece != "")
-                      return (
-                        <li className="font-ubuntu font-thin my-2 bulls">
-                          {piece}
-                        </li>
-                      );
-                  })}
-                </>
-              );
-            })}
         </div>
-      </div>
+      )}
       <Footer />
     </div>
   );

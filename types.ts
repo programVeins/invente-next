f69a0467df698862bbdc6f1819ea1ef4d787fbec
Harisaran <harisaran@motorq.com>
2022-10-16 18:44:35 +0530

    fix: event type

--- types.ts
+++ types.ts
@@ -1,40 +1,40 @@
 export enum EventPass {
-  Technical = "Technical",
-  NonTechnical = "Non-Technical",
-  Workshop = "Workshop",
-  Hackathon = "Hackathon",
+    Technical = "Technical",
+    NonTechnical = "Non-Technical",
+    Workshop = "Workshop",
+    Hackathon = "Hackathon",
 }
 
 export enum Department {
-  CSE = "Computer Science and Engineering",
-  IT = "Information Technology",
-  ECE = "ECE",
-  EEE = "Electrical and Electronics",
-  MECH = "Mechanical",
-  CIVIL = "Civil",
-  CHEM = "Chemical",
-  BME = "Biomedical",
-  BCOM = "BCOM",
+    CSE = "Computer Science and Engineering",
+    IT = "Information Technology",
+    ECE = "Electronics and Communication",
+    EEE = "Electrical and Electronics",
+    MECH = "Mechanical",
+    CIVIL = "Civil",
+    CHEM = "Chemical",
+    BME = "Biomedical",
+    BCOM = "BCOM",
 }
 
 export interface Section {
-  sectionName: string;
-  sectionData: string;
+    sectionName: string;
+    sectionData: string;
 }
 export enum EventType {
-  Individual = "Individual",
-  Team = "Team",
-  TeamOrSolo = "Team Or Solo",
+    Individual = "Individual",
+    Team = "Team",
+    TeamOrSolo = "Team Or Solo",
 }
 
 export interface Event {
-  id: string;
-  event_name: string;
-  description: string;
-  type: EventType;
-  size: string;
-  department: Department;
-  venue: string;
-  time: string;
-  sections: Record<string, any>;
+    id: string;
+    event_name: string;
+    description: string;
+    type: EventType;
+    size: string;
+    department: Department;
+    venue: string;
+    time: string;
+    sections: Record<string, any>;
 }

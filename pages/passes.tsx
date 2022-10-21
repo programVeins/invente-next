348cabef98651065f2a0354e9729268968143de8
Sabesh Bharathi <sabesh@rocketlane.com>
2022-10-21 14:31:31 +0530

    Passes page finish

--- pages/passes.tsx
+++ pages/passes.tsx
@@ -35,25 +35,27 @@ const Passes = () => {
           <h1 className="text-2xl md:text-5xl font-azonix text-white mt-40 text-center">
             Passes
           </h1>
-          <div className="grid gap-20 mt-20 mb-20 grid-cols-2 w-3/4 mx-auto text-white">
+          <div className="grid gap-20 mt-20 mb-20 grid-cols-1 lg:grid-cols-2 w-3/4 mx-auto text-white">
             {passes.map((pass, i) => (
-              <div
-                key={i}
-                className="w-full flex flex-col h-full py-10 px-20 justify-between flurry-bg text-center shadow-xl border-[0.02rem] border-gray-400 border-opacity-20 bg-black/20 rounded-md lg:rounded-xl backdrop-blur-lg"
-              >
-                <p className="font-aspace text-xl py-2">{pass.name}</p>
-                <p className="font-aspace text-5xl py-2 font-bold text-transparent bg-clip-text bg-gradient-to-br from-cyan-400 to-blue-600 shadow-blue-500">
-                  ₹{pass.amount}
-                </p>
-              </div>
+              <a href="https://axisbpayments.razorpay.com/ssn-snuc-Invente">
+                <div
+                  key={i}
+                  className="w-full cursor-pointer flex flex-col h-full py-10 px-20 justify-between flurry-bg text-center shadow-xl border-[0.02rem] border-gray-400 border-opacity-20 bg-black/20 rounded-md lg:rounded-xl backdrop-blur-lg"
+                >
+                  <p className="font-aspace text-xl py-2">{pass.name}</p>
+                  <p className="font-aspace text-5xl py-2 font-bold text-transparent bg-clip-text bg-gradient-to-br from-cyan-400 to-blue-600 shadow-blue-500">
+                    ₹{pass.amount}
+                  </p>
+                </div>
+              </a>
             ))}
           </div>
-          <p className="font-ubuntu text-2xl text-white text-center mb-4">
+          <p className="font-ubuntu px-10 lg:px-0 text-2xl text-white text-center mb-4">
             {" "}
             Please upload your receipts in this link!
           </p>
           <div className="flex w-full justify-center mb-20">
-            <Button>Upload</Button>
+            <Button disabled>Upload</Button>
           </div>
         </section>
       </main>

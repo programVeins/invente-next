9a9b4e3c28d7a4663f09ffa4549a6ec0fd5c4d28
Sabesh Bharathi <sabesh@rocketlane.com>
2022-10-27 11:11:10 +0530

    coundownmani

--- components/countdown/index.tsx
+++ components/countdown/index.tsx
@@ -31,7 +31,7 @@ const CountDown = () => {
   });
 
   return (
-    <div className="flex gap-4 px-2 lg:grid w-full lg:w-1/2 mx-auto lg:grid-cols-4 text-white font-azonix text-lg lg:text-3xl place-items-center">
+    <div className="flex gap-4 px-2 lg:grid w-full lg:w-3/4 xl:w-1/2 mx-auto lg:grid-cols-4 text-white font-azonix text-lg lg:text-3xl place-items-center">
       <div className="lg:h-28 lg:w-40 p-4 w-1/4 flex lg:flex-row flex-col items-center justify-center shadow-xl border-[0.02rem] border-gray-400 border-opacity-20 bg-black/20 rounded-md lg:rounded-xl backdrop-blur-lg">
         <p>{daysDiff}</p>
         <p className="text-xs lg:text-sm lg:mx-2">Days</p>

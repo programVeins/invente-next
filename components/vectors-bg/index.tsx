87f69f37365996e035606a0a03b53f034840a6aa
Sabesh Bharathi <sabesh@rocketlane.com>
2022-10-30 00:02:01 +0530

    vectors modile

--- components/vectors-bg/index.tsx
+++ components/vectors-bg/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function VectorsBG() {
   return (
-    <div className="fixed blur-[0.02rem] max-w-screen overflow-hidden lg:blur-[0.04rem]">
+    <div className="fixed blur-[0.04rem] max-w-screen overflow-hidden">
       <div className="">
         <img
           src="/vectors/gridbg.svg"
-          className="scale-[3] sm:scale-[2] md:scale-150 lg:scale-100"
+          className="scale-[5] sm:scale-[2] md:scale-150 lg:scale-100"
         />
       </div>
       <div className="mt-[150vw] sm:mt-[80vw] md:mt-[40vw] lg:mt-10">
